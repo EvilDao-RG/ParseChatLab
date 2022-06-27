7598c4a5c38e95fa5aed966d4859bb9660d1bf22
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-06-27 09:20:12 -0700

    Initial setup

---
+++ ParseChatLab/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ParseChatLab
+//
+//  Created by Gael Rodriguez Gomez on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
